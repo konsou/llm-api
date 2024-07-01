44a068eacfab918243c297366839a191d3aadf8b
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-07-01 15:43:56 +0300

    Add AnthropicApi to public API

--- llm_api/__init__.py
+++ llm_api/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 from .abc import LlmApi
+from .anthropic_api import AnthropicApi
 from .groq_api import GroqApi
 from .ollama_api import OllamaApi
 from .openrouter_api import OpenRouterAPI
